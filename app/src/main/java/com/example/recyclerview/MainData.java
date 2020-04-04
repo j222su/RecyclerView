5a4704ed69d74586c48b76bb05b3345663f718e3
j222su <patrickstar0910@gmail.com>
2020-04-04 17:06:11 +0900

    add constructor in MainData

--- app/src/main/java/com/example/recyclerview/MainData.java
+++ app/src/main/java/com/example/recyclerview/MainData.java
@@ -1,4 +1,28 @@
 package com.example.recyclerview;
 
 public class MainData {
+
+    private int imgProfile;
+    private String tvName;
+
+    public MainData(int imgProfile, String tvName) {
+        this.imgProfile=imgProfile;
+        this.tvName=tvName;
+    }
+
+    public int getImgProfile() {
+        return imgProfile;
+    }
+
+    public void setImgProfile(int imgProfile) {
+        this.imgProfile=imgProfile;
+    }
+
+    public String getTvName() {
+        return tvName;
+    }
+
+    public void setTvName(String tvName) {
+        this.tvName=tvName;
+    }
 }
