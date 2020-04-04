124eabea87e29ea03dde62031d8851f294190601
j222su <patrickstar0910@gmail.com>
2020-04-04 16:53:28 +0900

    add MainData class and add list_item.xml for list

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAdd=findViewById(R.id.btnAdd);
     }
 }
